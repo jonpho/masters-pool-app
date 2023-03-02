990d9abe540541700d268a36440997d265a6abf5
Jonathan Pho <jonpho@gmail.com>
2023-03-02 01:24:08 -0600

    Update ruby versions to fix rswag

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "2.7.0"
+ruby "3.2.1"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
@@ -48,15 +48,13 @@ gem "mysql2"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-
-gem "rswag-api"
-gem "rswag-ui"
+gem "rspec"
+gem "rswag"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "rspec-rails"
-  gem "rswag-specs"
+  gem "rspec-rails", '~> 5.0.0'
 end
 
 group :development do
---
+++ spec/requests/api/leaderboard_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'swagger_helper'
+
+describe 'Leaderboards API' do
+
+  path '/single_entry' do
+
+    post 'Create New entry' do
+      tags 'Single Entry'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :golfer_scorecard, in: :body, schema: {
+        type: :object,
+        properties: {
+          golfer_id: { type: :string },
+          year: { type: :integer },
+          position: { type: :integer },
+          round_one: { type: :integer },
+          round_two: { type: :integer },
+          round_three: { type: :integer },
+          round_four: { type: :integer },
+          total: { type: :integer },
+          earnings: { type: :integer }
+        },
+        required: [ 'golfer_id' ]
+      }
+
+      response '201', 'Leaderboard entry successful' do
+        let!(:golfer_scorecard) { { golfer_id: 1, year: 2023, round_one: 69 } }
+        schema type: :object,
+               properties: {
+                 message: { type: :string }
+               }
+        run_test!
+      end
+    end
+  end
+end
