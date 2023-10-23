58bbca2d985dcd4abb29ac255dffa9abbfef8786
Jonathan Pho <jonpho@gmail.com>
2023-10-23 03:19:56 -0500

    Added devise gem for authentication (#26)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :lockable
   include ActiveModel::SecurePassword
-  validates :username, :firstname, :lastname, :email, presence: true
-  has_secure_password
+  validates :email, presence: true
+  # has_secure_password
   # has_and_belongs_to_many :user_submissions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_030124) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_020429) do
   create_table "flights", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -59,10 +59,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_030124) do
     t.string "username"
     t.string "firstname"
     t.string "lastname"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
